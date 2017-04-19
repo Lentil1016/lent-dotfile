b4ec2cc587df76762e11ccbc3fe819d3ff2426e1
lentil1016 <lentil1016@gmail.com>
2017-04-19 10:20:39 +0800

    fix bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@ pushd ~
 mkdir .dotfile
 pushd .dotfile
 
+rm -f *
+
 wget https://raw.githubusercontent.com/Lentil1016/lent-dotfile/master/.zshrc
 wget https://raw.githubusercontent.com/Lentil1016/lent-dotfile/master/.gdbinit
 wget https://raw.githubusercontent.com/Lentil1016/lent-dotfile/master/.bashrc
